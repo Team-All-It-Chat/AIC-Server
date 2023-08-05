daf23548f0fea2561bd56f97b23f5fbd8f2f9dfc
eunsu02 <regulus_leo08@naver.com>
2023-08-05 21:14:08 +0900

    [FIX] csrf 에러

--- config/settings.py
+++ config/settings.py
@@ -111,12 +111,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+
+CSRF_TRUSTED_ORIGINS = ["https://all-it-chat.o-r.kr/"]
 
 CORS_ALLOW_CREDENTIALS = True
 
