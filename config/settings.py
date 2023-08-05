2d4267e0a246aa2a165d3dc5a1daa163edbfc6e7
eunsu02 <regulus_leo08@naver.com>
2023-08-06 01:03:07 +0900

    [FIX] CORS_ALLOWED_ORIGINS 수정

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://all-it-chat.o-r.kr/"
+    "https://all-it-chat.o-r.kr"
 ]
 
 ROOT_URLCONF = 'config.urls'
