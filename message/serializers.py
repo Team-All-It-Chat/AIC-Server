dc9bc1d14f7a8ebf4c7c4f76220ba36cd552bb0d
eunsu02 <regulus_leo08@naver.com>
2023-08-09 19:25:28 +0900

    [FIX] 중간 점검

--- message/serializers.py
+++ message/serializers.py
@@ -22,9 +22,9 @@ class ChatUpdateStatusSerializer(serializers.ModelSerializer):
 
 class ChatSerializer(serializers.ModelSerializer):
     question_time = serializers.DateTimeField(read_only=True)
-    answer = serializers.TextField(read_only=True)
+    answer = serializers.CharField(read_only=True)
     image=serializers.ImageField()
-    answer_time = serializers.DateTimeField(blank=True, null=True)
+    answer_time = serializers.DateTimeField(required=False, allow_null=True)
     class Meta:
         model = Chat
         fields = '__all__'
@@ -33,11 +33,13 @@ class ChatSerializer(serializers.ModelSerializer):
         # 현재 로그인한 사용자 가져오기
         questioner = self.context['request'].user
 
-        # 현재 시간을 설정하여 question_time에 저장
-        validated_data['question_time'] = timezone.now()
-
         # "질문자" 필드를 현재 로그인한 사용자로 설정
         validated_data['questioner'] = questioner
+        #validated_data['questioner'] = 1
+        
+
+        # 현재 시간을 설정하여 question_time에 저장
+        validated_data['question_time'] = timezone.now()
 
         # Chat 인스턴스 생성
         chat = Chat.objects.create(**validated_data)
@@ -51,7 +53,7 @@ import boto3
 from botocore.exceptions import ClientError
 from config.settings import AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME
 
-VALID_IMAGE_EXTENSIONS = [ "jpg", "jpeg", "png", "gif", ]
+# VALID_IMAGE_EXTENSIONS = [ "jpg", "jpeg", "png", "gif", ]
 
 
 from rest_framework import serializers
@@ -63,28 +65,28 @@ class ChatUpdateSerializer(serializers.ModelSerializer):
         model = Chat
         fields = '__all__'
       
-    def validate(self, data): 
-        image = data.get('image')
+    # def validate(self, data): 
+    #     image = data.get('image')
 
-        if not image.name.split('.')[-1].lower() in VALID_IMAGE_EXTENSIONS:
-            raise serializers.ValidationError("Not an Image File")
+    #     if not image.name.split('.')[-1].lower() in VALID_IMAGE_EXTENSIONS:
+    #         raise serializers.ValidationError("Not an Image File")
 
-        return data
+    #     return data
 
     def update(self, instance, validated_data):
         # 이미지를 S3 버킷으로 바로 업로드하기 위해 이미지 처리 추가
-        image_file = validated_data.get('image')
-        if image_file:
-            s3 = boto3.client('s3',
-                aws_access_key_id = AWS_ACCESS_KEY_ID,
-                aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-                region_name = AWS_REGION)
-            try:
-                s3.upload_fileobj(image_file, AWS_STORAGE_BUCKET_NAME, image_file.name)  # 파일 업로드
-                img_url = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{image_file.name}"
-                instance.image = img_url
-            except:
-                raise serializers.ValidationError("Invalid Image File")
+        # image_file = validated_data.get('image')
+        # if image_file:
+        #     s3 = boto3.client('s3',
+        #         aws_access_key_id = AWS_ACCESS_KEY_ID,
+        #         aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+        #         region_name = AWS_REGION)
+        #     try:
+        #         s3.upload_fileobj(image_file, AWS_STORAGE_BUCKET_NAME, image_file.name)  # 파일 업로드
+        #         img_url = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{image_file.name}"
+        #         instance.image = img_url
+        #     except:
+        #         raise serializers.ValidationError("Invalid Image File")
 
         # answer_time 자동 설정
         instance.answer_time = timezone.now()
--- message/urls.py
+++ message/urls.py
@@ -3,5 +3,10 @@ from .views import *
 
 urlpatterns = [
     #path('create-question/', QuestionCreateView.as_view(), name='create_question'),
-
+    path('q/',QuestionCreateView.as_view()),
+    path('qq/',question.as_view()),
+    
+    path('',MyAPIViews.as_view()),
+    path('status/',ChatUpdateStatusAPIView.as_view()),
+    path('recent/',RecentQuestionAPIView.as_view())
     ]
